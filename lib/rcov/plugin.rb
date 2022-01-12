01f07e7ff93b44a5340158dff6c825c623e3720d
mbehboodian <29199390+xeptore@users.noreply.github.com>
2022-01-12 18:34:05 +0330

    fix: replace circleci wrapper gem with own implementation

--- lib/rcov/plugin.rb
+++ lib/rcov/plugin.rb
@@ -1,20 +1,31 @@
 # frozen_string_literal: false
 
-require 'open-uri'
-require 'net/http'
-require 'circle_ci_wrapper'
+require "open-uri"
+require "net/http"
+require "circle_ci_wrapper"
 
 module Danger
   class DangerRcov < Plugin
     # report will get the urls from circleCi trough circle_ci_wrapper gem
-    def report(branch_name = 'master', build_name = 'build', show_warning = true)
+    def report(branch_name = "master", build_name = "build", show_warning = true)
       puts "Start debugging for branch_name = #{branch_name}, build_name = #{build_name}..."
 
-      current_url, master_url = CircleCiWrapper.report_urls_by_branch(branch_name, build_name)
-      "#{CIRCLE_CI_API_URL}/#{ENV['CIRCLE_PROJECT_USERNAME']}/#{ENV['CIRCLE_PROJECT_REPONAME']}"
+      api = "https://circleci.com/api/v1.1/project/github"
 
-      p current_url
-      p master_url
+      gh_project = ENV["CIRCLE_PROJECT_USERNAME"]
+      gh_repo = ENV["CIRCLE_PROJECT_REPONAME"]
+      repo_url = "#{api}/#{gh_project}/#{gh_repo}"
+      circleci_token = ENV["CIRCLE_TOKEN"]
+      url = "#{repo_url}/tree/#{branch_name}?circle-token=#{circleci_token}&limit=6&filter=completed"
+
+      res = Net::HTTP.get_response(URI.parse(url))
+
+      puts res.code
+      puts res.body
+
+      latest_build_num = latest_build(url, build_name)
+
+      puts latest_build_num
 
       report_by_urls(current_url, master_url, show_warning)
     end
@@ -33,8 +44,8 @@ module Danger
       print "@master_report: "
       p @master_report
 
-      if show_warning && @master_report && @master_report.dig('metrics', 'covered_percent').round(2) > @current_report.dig('metrics', 'covered_percent').round(2)
-        warn("Code coverage decreased from #{@master_report.dig('metrics', 'covered_percent').round(2)}% to #{@current_report.dig('metrics', 'covered_percent').round(2)}%")
+      if show_warning && @master_report && @master_report.dig("metrics", "covered_percent").round(2) > @current_report.dig("metrics", "covered_percent").round(2)
+        warn("Code coverage decreased from #{@master_report.dig("metrics", "covered_percent").round(2)}% to #{@current_report.dig("metrics", "covered_percent").round(2)}%")
       end
 
       # Output the processed report
@@ -42,6 +53,12 @@ module Danger
     end
 
     private
+
+    def latest_build(url, build_name)
+      JSON.parse(URI.parse(url).read).select do |build|
+        build&.[]("build_parameters")&.[]("CIRCLE_JOB") == build_name
+      end&.first&.[]("build_num")
+    end
 
     def get_report(url:)
       JSON.parse(URI.parse(url).read) if url
@@ -52,30 +69,30 @@ module Danger
       p results
       print "master_results: "
       p master_results
-      @current_covered_percent = results&.dig('metrics', 'covered_percent')&.round(2)
-      @current_files_count = results&.dig('files')&.count
-      @current_total_lines = results&.dig('metrics', 'total_lines')
+      @current_covered_percent = results&.dig("metrics", "covered_percent")&.round(2)
+      @current_files_count = results&.dig("files")&.count
+      @current_total_lines = results&.dig("metrics", "total_lines")
       print "@current_total_lines: "
       p @current_total_lines
-      @current_misses_count = @current_total_lines - results&.dig('metrics', 'covered_lines')
+      @current_misses_count = @current_total_lines - results&.dig("metrics", "covered_lines")
 
       if master_results
-        @master_covered_percent = master_results&.dig('metrics', 'covered_percent')&.round(2)
-        @master_files_count = master_results.dig('files')&.count
-        @master_total_lines = master_results.dig('metrics', 'total_lines')
-        @master_misses_count = @master_total_lines - master_results.dig('metrics', 'covered_lines')
+        @master_covered_percent = master_results&.dig("metrics", "covered_percent")&.round(2)
+        @master_files_count = master_results.dig("files")&.count
+        @master_total_lines = master_results.dig("metrics", "total_lines")
+        @master_misses_count = @master_total_lines - master_results.dig("metrics", "covered_lines")
       end
 
       message = "```diff\n@@           Coverage Diff            @@\n"
-      message << "## #{justify_text('master', 16)} #{justify_text('#' + ENV['CIRCLE_PULL_REQUEST'].split('/').last, 8)} #{justify_text('+/-', 7)} #{justify_text('##', 3)}\n"
+      message << "## #{justify_text("master", 16)} #{justify_text("#" + ENV["CIRCLE_PULL_REQUEST"].split("/").last, 8)} #{justify_text("+/-", 7)} #{justify_text("##", 3)}\n"
       message << separator_line
-      message << new_line('Coverage', @current_covered_percent, @master_covered_percent, '%')
+      message << new_line("Coverage", @current_covered_percent, @master_covered_percent, "%")
       message << separator_line
-      message << new_line('Files', @current_files_count, @master_files_count)
-      message << new_line('Lines', @current_total_lines, @master_total_lines)
+      message << new_line("Files", @current_files_count, @master_files_count)
+      message << new_line("Lines", @current_total_lines, @master_total_lines)
       message << separator_line
-      message << new_line('Misses', @current_misses_count, @master_misses_count)
-      message << '```'
+      message << new_line("Misses", @current_misses_count, @master_misses_count)
+      message << "```"
     end
 
     def separator_line
@@ -83,29 +100,29 @@ module Danger
     end
 
     def new_line(title, current, master, symbol = nil)
-      formatter = symbol ? '%+.2f' : '%+d'
+      formatter = symbol ? "%+.2f" : "%+d"
       currrent_formatted = current.to_s + symbol.to_s
-      master_formatted = master ? master.to_s + symbol.to_s : '-'
+      master_formatted = master ? master.to_s + symbol.to_s : "-"
       prep = calulate_prep(master_formatted, current - master)
 
       line = data_string(title, master_formatted, currrent_formatted, prep)
-      line << justify_text(format(formatter, current - master) + symbol.to_s, 8) if prep != '  '
+      line << justify_text(format(formatter, current - master) + symbol.to_s, 8) if prep != "  "
       line << "\n"
       line
     end
 
-    def justify_text(string, adjust, position = 'right')
-      string.send(position == 'right' ? :rjust : :ljust, adjust)
+    def justify_text(string, adjust, position = "right")
+      string.send(position == "right" ? :rjust : :ljust, adjust)
     end
 
     def data_string(title, master, current, prep)
-      "#{prep}#{justify_text(title, 9, 'left')} #{justify_text(master, 7)}#{justify_text(current, 9)}"
+      "#{prep}#{justify_text(title, 9, "left")} #{justify_text(master, 7)}#{justify_text(current, 9)}"
     end
 
     def calulate_prep(master_formatted, diff)
-      return '  ' if master_formatted != '-' && diff.zero?
+      return "  " if master_formatted != "-" && diff.zero?
 
-      diff.positive? ? '+ ' : '- '
+      diff.positive? ? "+ " : "- "
     end
   end
 end
